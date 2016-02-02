623f79b2008bd5fba184664b2f30a88bc3a4cef8
Jesus Flores <joy_warmgun@hotmail.com>
2016-02-01 22:37:17 -0600

    Clase 2

--- puls5/ShoppyProy/products/models.py
+++ puls5/ShoppyProy/products/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from clients.models import Client
 
 # Create your models here.
 class Product(models.Model):
@@ -11,3 +12,17 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		ordering = ('id', )
+
+class Favorite(models.Model):
+	user = models.ForeignKey(Client)
+	product = models.ForeignKey(Product)
+
+	class Meta:
+		verbose_name = 'Favorite'	
+		verbose_name_plural = 'Favorites'
+
+	def __str__(self):
+		return '%s %s' % (self.user.name, self.product.name)
--- puls5/ShoppyProy/products/urls.py
+++ puls5/ShoppyProy/products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.hello_world, name="hello"),
+	url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail),
+	url(r'^product/new/$', views.new_product),
 ]
